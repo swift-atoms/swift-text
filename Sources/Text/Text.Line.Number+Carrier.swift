2ff960d6d9c0e7f50784d2674ce021afd779c5b2
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-10 09:40:45 +0200

    Keep essential package source and tests

--- Sources/Text/Text.Line.Number+Carrier.swift
+++ Sources/Text/Text.Line.Number+Carrier.swift
@@ -1,6 +1,5 @@
 public import Carrier
 
-/// Exposes a text line number's `UInt` representation through `Carrier`.
 extension Text.Line.Number: Carrier.`Protocol` {
 
     public typealias Underlying = UInt
